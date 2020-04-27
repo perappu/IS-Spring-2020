f77b765ce99f0696a7e1029d0e846b4f22407cae
Kazi Fodor <stokoriel@gmail.com>
2020-04-27 08:08:14 -0500

    i really don't know why the url is causing problems

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Kazimir Fodor'
 SITENAME = 'IS 2020'
-SITEURL = 'https://stokori.github.io/IS-Spring-2020'
+SITEURL = 'https://interdisciplinary2020.herokuapp.com/'
 
 PATH = 'content'
 
