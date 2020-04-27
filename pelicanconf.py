9f0d043083b8be5ba53f1315be34f84238412eb9
Kazi Fodor <stokoriel@gmail.com>
2020-04-27 15:41:07 -0500

    fix urls

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Kazimir Fodor'
 SITENAME = 'IS 2020'
-SITEURL = 'https://interdisciplinary2020.herokuapp.com/'
+SITEURL = 'https://quarantinejournal.herokuapp.com/'
 
 PATH = 'content'
 
